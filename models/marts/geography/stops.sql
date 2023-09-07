57e69861ecf4bffcd06f978bd38fdd5a32577027
jrgarrar <jgarrard@resourcedata.com>
2023-09-07 15:25:43 -0600

    Patched stops, added exploration notebook

--- models/marts/geography/stops.sql
+++ models/marts/geography/stops.sql
@@ -5,7 +5,6 @@
 with
 
 stops as (
-
     select 
         "STOP_ID" as "stop_id",
         "STOP_LAT" as "stop_latitude",
@@ -13,26 +12,9 @@ stops as (
         "STOP_NAME" as "stop_name"
     from 
         STOPS_SEED
-),
-
-stop_features as (
-
-    select 
-        "STOP_ID" as "stop_id",
-        "FEATURE_NAME" as "feature_name"
-    from 
-        STOP_FEATURES_SEED
 )
 
 select 
-    stops."stop_id",
-    stops."stop_latitude",
-    stops."stop_longitude",
-    stops."stop_name",
-    stop_features."feature_name"
-from 
+    *
+from
     stops
-left join
-    stop_features
-on
-    stops."stop_id" = stop_features."stop_id"
